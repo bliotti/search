aa7fc37ce56231427c8f20d3a238f95b7a4d2255
Brian Liotti <bliotti@protonmail.com>
2021-08-13 00:22:58 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import useFetch from './hooks/useFetch'
+
+import House from './components/House'
+
+export default function App() {
+  const { data, setData } = useFetch()
+  return (
+    <main>
+      <input
+        type='text'
+        placeholder='fav house'
+        value={data.slug}
+        onChange={(e) => setData({ ...data, slug: e.target.value })}
+      />
+      <br />
+      {data.results.length > 0 ? <House family={data.results[0]} /> : null}
+    </main>
+  )
+}
